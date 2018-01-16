6e200adccc26a14e88e2b8b69a99c4e2a9f453f0
weiguangping <757846526@qq.com>
2018-01-16 21:20:37 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var webpack = require('webpack')
 module.exports = {
   entry: {
+    vendor: ['jquery', './src/js/common.js'],
     index: './src/js/index.js',
     cart: './src/js/cart.js'
   },
@@ -20,7 +21,12 @@ module.exports = {
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [
-            {loader: 'css-loader'}
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true // css压缩
+              }
+            }
           ]
         })
       },
@@ -39,20 +45,38 @@ module.exports = {
       verbose: true,
       dry: false
     }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      chunks: ['index', 'cart', 'vendor'],
+      minChunks: 3
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html',
-      chunks: ['index']
+      chunks: ['index', 'vendor'],
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
     }),
     new HtmlWebpackPlugin({
       filename: 'cart.html',
       template: './src/cart.html',
-      chunks: ['cart']
+      chunks: ['cart', 'vendor'],
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
     }),
     new webpack.ProvidePlugin({
       $: 'jquery'
     }),
-    new ExtractTextPlugin('css/styles.css')
+    new ExtractTextPlugin('css/index.css')
   ]
   // devtool: '#souerce-map'
 }
